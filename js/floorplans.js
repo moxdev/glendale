4cde31432662ea93c019b536dee26a8896401ddd
Mox Biggs <moxdev1@gmail.com>
2017-03-08 13:59:41 -0500

    refactored floorplans

--- js/floorplans.js
+++ js/floorplans.js
@@ -1,23 +1,23 @@
-// jQuery(window).load(function() {
+jQuery(window).load(function() {
 
-//     if(jQuery(".view-btn")[0]) {
-//         jQuery(".view-hide-wrapper").css("display", "none");
-//     }else {
-//         jQuery(".view-hide-wrapper").css("display", "block");
-//     }
+    if(jQuery(".view-btn")[0]) {
+        jQuery(".view-hide-wrapper").css("display", "none");
+    }else {
+        jQuery(".view-hide-wrapper").css("display", "block");
+    }
 
-//     var floor_plan_expand = function() {
-//         var toShow = jQuery(this).next();
+    var floor_plan_expand = function() {
+        var toShow = jQuery(this).next();
 
-//         if( jQuery(toShow).hasClass('active') ) {
-//             jQuery(toShow).removeClass('active');
-//             jQuery(this).html('View Floor Plans +');
-//         } else {
-//             jQuery(toShow).addClass('active');
-//             jQuery(this).html('Hide Floor Plans -');
-//         }
-//     };
+        if( jQuery(toShow).hasClass('active') ) {
+            jQuery(toShow).removeClass('active');
+            jQuery(this).html('View Floor Plans +');
+        } else {
+            jQuery(toShow).addClass('active');
+            jQuery(this).html('Hide Floor Plans -');
+        }
+    };
 
-//     jQuery('.fp-models .view-btn').on('click', floor_plan_expand);
-// });
+    jQuery('.fp-models .view-btn').on('click', floor_plan_expand);
+});
 
